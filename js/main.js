c868bbb0184d1db5de7d4b942326d58332a5060e
Martin Alvarado <martin.alvarado2002@gmail.com>
2022-01-22 16:54:01 +0100

    Creating Model

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
-
 // In the first few sections, we do all the coding here.
 // Later, you'll see how to organize your code into separate
 // files and modules.
+
+let Song = Backbone.Model.extend({
+  initialize: function () {
+    console.log("A new song has been created");
+  },
+});
+
+let song = new Song();

f14ef64bd2f23495b69f8d4980ef9cdf79b80986
Martin Alvarado <martin.alvarado2002@gmail.com>
2022-01-27 17:57:01 +0100

    Inheritance

--- js/main.js
+++ js/main.js
@@ -2,23 +2,19 @@
 // Later, you'll see how to organize your code into separate
 // files and modules.
 
-let Song = Backbone.Model.extend({
-  initialize: function () {
-    console.log("A new song has been created");
-  },
-  validate: function (attrs) {
-    if (!attrs.title) {
-      return "Title is required";
-    }
+let Animal = Backbone.Model.extend({
+  walk: function () {
+    console.log("Animal walking...");
   },
 });
 
-var song = new Song();
-if (!song.isValid()) {
-  console.log("Error description: ", song.validationError);
-}
+let Dog = Animal.extend({
+  walk: function () {
+    Animal.prototype.walk.apply(this);
+    console.log("Dog walking...");
+  },
+});
 
-song = new Song({ title: "Rock 'n Roll Widow" });
-if (song.isValid()) {
-  console.log("Song title: ", song.attributes.title);
-}
+let dog = new Dog();
+
+dog.walk();

981d8b7b63cf4edc77c79caee54523cdbe4c3de4
Martin Alvarado <martin.alvarado2002@gmail.com>
2022-01-28 22:10:56 +0100

    Working with collections

--- js/main.js
+++ js/main.js
@@ -8,10 +8,27 @@ let Songs = Backbone.Collection.extend({
   model: Song,
 });
 
-let songs = new Songs([
-  new Song({ title: "Song 1" }),
-  new Song({ title: "Song 2" }),
-  new Song({ title: "Song 3" }),
-]);
+let songs = new Songs();
 
-songs.add(new Song({ title: "Song 4" }));
+songs.add(
+  new Song({
+    title: "Song 1",
+    genre: "Jazz",
+    downloads: 110,
+  }),
+  { at: 0 }
+);
+
+songs.push(new Song({ title: "Song 2", genre: "Jazz", downloads: 90 }));
+
+let jazzSongs = songs.where({ genre: "Jazz" });
+let firstJazzSong = songs.findWhere({ genre: "Jazz" });
+
+console.log("Jazz Songs", jazzSongs);
+console.log("First Jazz Songs", firstJazzSong);
+
+let filteredSongs = songs.where({ genre: "Jazz" }, { title: "Song 2" });
+console.log("Filtered Songs", filteredSongs);
+
+let topDownloads = songs.filter(song => song.get("downloads") > 100);
+console.log("Top Songs", topDownloads);

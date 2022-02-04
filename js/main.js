7c5f97f1350f6a2b4c494d603ba48cd219a72229
Martin Alvarado <martin.alvarado2002@gmail.com>
2022-02-04 21:16:18 +0100

    Routers

--- js/main.js
+++ js/main.js
@@ -4,3 +4,62 @@ var ArtistsView = Backbone.View.extend({
     return this;
   },
 });
+
+var AlbumsView = Backbone.View.extend({
+  render: function () {
+    this.$el.html('ALBUMS VIEW');
+    return this;
+  },
+});
+
+var GenresView = Backbone.View.extend({
+  render: function () {
+    this.$el.html('GENRES VIEW');
+    return this;
+  },
+});
+
+var AppRouter = Backbone.Router.extend({
+  routes: {
+    'albums': 'viewAlbums',
+    'albums/:albumId': 'viewAlbumById',
+    'artists': 'viewArtists',
+    'genres': 'viewGenres',
+    '*other': 'defaultRoute',
+  },
+
+  viewAlbums: function () {
+    var view = new AlbumsView({ el: '#container' });
+    view.render();
+  },
+  viewAlbumById: function (albumId) {
+    //
+  },
+  viewArtists: function () {
+    let view = new ArtistsView({ el: '#container' });
+    view.render();
+  },
+  viewGenres: function () {
+    let view = new GenresView({ el: '#container' });
+    view.render();
+  },
+  defaultRoute: function () {
+    let view = new AlbumsView({ el: '#container' });
+    view.render();
+  },
+});
+
+var router = new AppRouter();
+Backbone.history.start();
+
+var NavView = Backbone.View.extend({
+  events: {
+    'click': 'onClick',
+  },
+  onClick: function (e) {
+    var $li = $(e.target);
+    router.navigate($li.attr('data-url'), { trigger: true });
+  },
+});
+
+var navView = new NavView({ el: '#nav' });

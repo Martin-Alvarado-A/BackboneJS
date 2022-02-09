4455e916eda72ba0736323bc358b294aa16c185d
Martin Alvarado <martin.alvarado2002@gmail.com>
2022-02-09 22:11:24 +0100

    Modularization initial

--- js/main.js
+++ js/main.js
@@ -1,65 +1,14 @@
-var ArtistsView = Backbone.View.extend({
+var Song = Backbone.Model.extend();
+
+var SongView = Backbone.View.extend({
   render: function () {
-    this.$el.html('ARTISTS VIEW');
+    this.$el.html(this.model.get('title'));
     return this;
   },
 });
 
-var AlbumsView = Backbone.View.extend({
-  render: function () {
-    this.$el.html('ALBUMS VIEW');
-    return this;
-  },
-});
+var song = new Song({ title: 'blue in Green' });
 
-var GenresView = Backbone.View.extend({
-  render: function () {
-    this.$el.html('GENRES VIEW');
-    return this;
-  },
-});
+var songView = new SongView({ el: '#container', model: song });
 
-var AppRouter = Backbone.Router.extend({
-  routes: {
-    'albums': 'viewAlbums',
-    'albums/:albumId': 'viewAlbumById',
-    'artists': 'viewArtists',
-    'genres': 'viewGenres',
-    '*other': 'defaultRoute',
-  },
-
-  viewAlbums: function () {
-    var view = new AlbumsView({ el: '#container' });
-    view.render();
-  },
-  viewAlbumById: function (albumId) {
-    //
-  },
-  viewArtists: function () {
-    let view = new ArtistsView({ el: '#container' });
-    view.render();
-  },
-  viewGenres: function () {
-    let view = new GenresView({ el: '#container' });
-    view.render();
-  },
-  defaultRoute: function () {
-    let view = new AlbumsView({ el: '#container' });
-    view.render();
-  },
-});
-
-var router = new AppRouter();
-Backbone.history.start();
-
-var NavView = Backbone.View.extend({
-  events: {
-    'click': 'onClick',
-  },
-  onClick: function (e) {
-    var $li = $(e.target);
-    router.navigate($li.attr('data-url'), { trigger: true });
-  },
-});
-
-var navView = new NavView({ el: '#nav' });
+songView.render();
